a0362082cbbd4fffbde0a7a9bb26ba1ba8f412a9
cheetiantey <37930535+cheetiantey@users.noreply.github.com>
2021-08-10 16:15:31 -0500

    Add User registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-// @router  GET api/users
-// @ desc   Test route
+// @router  POST api/users
+// @ desc   Register user
 // @access  public
-router.get('/', (req, res) => res.send('User route'));
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a password with 8 or more characters').isLength({ min: 6 })
+], (req, res) => {
+    const errors = validationResult(req);
+
+    // Return a status code of 400 if it doesn't passes the check
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    res.send('User route');
+});
 
 module.exports = router;
