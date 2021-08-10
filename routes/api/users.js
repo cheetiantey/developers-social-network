eb37f2c1e12d69e124eda44ac4f329636f188a97
cheetiantey <37930535+cheetiantey@users.noreply.github.com>
2021-08-10 16:43:32 -0500

    Fix User registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
+
+const User = require('../../models/User');
 
 // @router  POST api/users
 // @ desc   Register user
@@ -9,13 +13,54 @@ router.post('/', [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please enter a valid email').isEmail(),
     check('password', 'Please enter a password with 8 or more characters').isLength({ min: 6 })
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req);
 
     // Return a status code of 400 if it doesn't passes the check
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
     }
+
+    // Destructure req
+    const { name, email, password } = req.body;
+
+    try {
+        // Check if the user already exists
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({errors: [{ msg: "User already exists" }]});
+        }
+    
+        // Gets the user's gravatar
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        })
+
+        user = new User({
+            name, 
+            email,
+            avatar,
+            password
+        });
+
+        // Encrypt password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        // Saves the user
+        await user.save();
+
+        // Todo: Return jsonwebtoken 
+        res.send('User registered')
+
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    } 
+
 
     res.send('User route');
 });
