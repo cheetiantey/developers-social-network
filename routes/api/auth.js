b025f60390e9e3454529429646359a4a688969fe
cheetiantey <37930535+cheetiantey@users.noreply.github.com>
2021-08-14 00:37:29 -0500

    Add Middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 // @router  GET api/auth
 // @ desc   Test route
-// @access  public
-router.get('/', (req, res) => res.send('Auth route'));
+// @access  Public
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
 
 module.exports = router;
